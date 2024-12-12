a1d30b863a4f71de1e182985bb1fa0b56d3b3161
Rohaan02 <rohaannadeem2@gmail.com>
2024-12-12 23:28:08 +0500

    made post crud.

--- adonis_learning/database/migrations/1733823453653_create_posts_table.ts
+++ adonis_learning/database/migrations/1733823453653_create_posts_table.ts
@@ -5,7 +5,16 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments('id').notNullable().primary()
+      table
+        .integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+
+      table.string('content')
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
--- adonis_learning/start/routes.ts
+++ adonis_learning/start/routes.ts
@@ -11,7 +11,7 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 
 const UsersController = () => import('#controllers/users_controller')
-
+const PostsController = () => import('#controllers/posts_controller')
 
 // ** previous by default router starts here
 router.get('/', async () => {
@@ -21,18 +21,32 @@ router.get('/', async () => {
 })
 // previous by default router ends here **
 
-router.group(() => {
-  router.post('/register', [UsersController, 'register'])
-  router.post('/login', [UsersController, 'login'])
-}).prefix('/user')
+router
+  .group(() => {
+    router.post('/register', [UsersController, 'register'])
+    router.post('/login', [UsersController, 'login'])
+  })
+  .prefix('/user')
+
+router
+  .group(() => {
+    router.post('/store', [PostsController, 'store'])
+    router.get('/index', [PostsController, 'index'])
+    router.get('/show/:id', [PostsController, 'show'])
+    router.put('/update/:id', [PostsController, 'update'])
+    router.delete('/destroy/:id', [PostsController, 'destroy'])
+  })
+  .use(middleware.auth())
+  .prefix('/post')
 
 // add this route
-router.get('me', async ({ auth, response }) => {
-  try {
-    const user = auth.getUserOrFail()
-    return response.ok(user)
-  } catch (error) {
-    return response.unauthorized({ error: 'User not found' })
-  }
-})
-.use(middleware.auth())
+router
+  .get('me', async ({ auth, response }) => {
+    try {
+      const user = auth.getUserOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.unauthorized({ error: 'User not found' })
+    }
+  })
+  .use(middleware.auth())
